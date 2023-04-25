6c43dd96886d8092d8950ad6a2b274297b5fb696
rluispdev <rluisp@icloud.com>
2023-04-25 17:00:19 -0300

    Initial Commit

---
+++ bomSaber/bomSaberApp.swift
@@ -0,0 +1,17 @@
+//
+//  bomSaberApp.swift
+//  bomSaber
+//
+//  Created by Rafael Gonzaga on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct bomSaberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
